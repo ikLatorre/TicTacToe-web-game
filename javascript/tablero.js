758fc4cff01a2fb68b652a1301eecae2cb35e229
ikLatorre <ikLatorre@IakiWin7>
2014-11-26 18:10:47 +0100

    Primera prueba local.

--- javascript/tablero.js
+++ javascript/tablero.js
@@ -5,20 +5,105 @@
  */
 
 function hacerMovimiento(posicion){
-    document.getElementById("p" + posicion).innerHTML = "X";
+    document.getElementById("pos" + posicion).innerHTML = "O";
+    document.getElementById("div" + posicion).removeAttribute("onclick");
+    bloquearTablero();
+    document.getElementById("resultado").innerHTML = "Calculando resultado...";
     
     var XHRObject = new XMLHttpRequest();
     XHRObject.open("GET", "movimiento.php?pos="+posicion, "true");
-	//XHRObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	//XHRObject.setRequestHeader("Content-length", params.length);
 	XHRObject.send();
-	
-	XHRObject.onreadystatechange = function()
-	{
-            if (XHRObject.readyState == 4)
-		{
-                    var obj = document.getElementById("p" + XHRObject.responseText);
-                    obj.innerHTML = "X";
-		}
+	XHRObject.onreadystatechange = function(){
+            if (XHRObject.readyState == 4){
+                document.getElementById("pos" + XHRObject.responseText).innerHTML = "X";
+                document.getElementById("div" + XHRObject.responseText).removeAttribute("onclick");
+                desbloquearTablero();
+                document.getElementById("resultado").innerHTML = "";
+            }
 	}
+}
+
+function bloquearTablero(){
+    alert('aa');
+    var obj;
+    obj = document.getElementById("div00");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+
+    
+    obj = document.getElementById("div01");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div02");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div10");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div11");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div12");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div20");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div21");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div22");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+    
+    obj = document.getElementById("div00");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", null);
+}
+
+function desbloquearTablero(){
+    var obj;
+    
+    obj = document.getElementById("div00");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('00'));
+    
+    obj = document.getElementById("div01");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('01'));
+    
+    obj = document.getElementById("div02");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('02'));
+    
+    obj = document.getElementById("div10");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('10'));
+    
+    obj = document.getElementById("div11");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('11'));
+    
+    obj = document.getElementById("div12");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('12'));
+    
+    obj = document.getElementById("div20");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('20'));
+    
+    obj = document.getElementById("div21");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('21'));
+    
+    obj = document.getElementById("div22");
+    if(obj.hasAttribute("onclick"))
+        obj.setAttribute("onclick", hacerMovimiento('22'));
 }
